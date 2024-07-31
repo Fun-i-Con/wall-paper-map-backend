1fe2477a6e1ff58d897106bc74c07975025588fd
codepro2020MBP04 <codepro2020mbp04@codepro2020MBP04s-MacBook-Pro.local>
2024-07-31 19:39:06 +0900

    cors

--- api/main.py
+++ api/main.py
@@ -1,7 +1,4 @@
 from fastapi import FastAPI
-
-from api.routers import data
-
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -10,9 +7,9 @@ app.include_router(data.router)
 origins = [
     "http://localhost",
     "http://localhost:3000",
-    "http://127.0.0.1:5500",  # 必要に応じて追加
-    "http://18.179.199.216/", # 実際のドメインを追加
-    "https://wall-paper-map-backend-nsc2gqv76q-uc.a.run.app/" # Cloud Runのドメインを追加
+    "http://127.0.0.1:5500",
+    "http://18.179.199.216/member-page/kabegami-map/index.html",
+    "https://wall-paper-map-backend-nsc2gqv76q-uc.a.run.app"
 ]
 
 app.add_middleware(
